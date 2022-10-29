47b51a3ee9aae4d5da9e85704bbda9f802f2b17b
subzero161 <58404587+subzero161@users.noreply.github.com>
2022-10-28 21:03:06 -0400

    dummy py update

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -1,26 +1,98 @@
-## Correct way example here:  https://pypi.org/project/python-dotenv/ 
-# use dotenv to load in environment variables
-sudo apt-get install python3-dev default-libmysqlclient-dev 
-pip install pymysql sqlalchemy
+#Check the ipynb file 
 
+
+#import packages
 from sqlalchemy import create_engine
 import sqlalchemy
 from dotenv import load_dotenv
 import os
 load_dotenv()
+
+
 MYSQL_HOSTNAME = os.getenv("MYSQL_HOSTNAME")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
-
-########
-
 connection_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}:3306/{MYSQL_DATABASE}'
 connection_string
 
-engine = create_engine(connection_string)
+db_gcp = create_engine(connection_string)
 
-#TABLENAME = MYSQL_USER + 'fakeTableAssignment1'
+### show tables from databases
+tableNames_gcp = db_gcp.table_names()
 
-#fakeDataset.to_sql(TABLENAME, con=engine)
+# reoder tables: production_patient_conditions, production_patient_medications, production_medications, production_patients, production_conditions
+tableNames_gcp = ['production_patient_conditions', 'production_patient_medications', 'production_medications', 'production_patients', 'production_conditions']
+
+table_patients = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    contact_mobile varchar(255) default null,
+    contact_home varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+
+table_medications = """
+create table if not exists production_medications (
+    id int auto_increment,
+    med_ndc varchar(255) default null unique,
+    med_human_name varchar(255) default null,
+    med_is_dangerous varchar(255) default null,
+    PRIMARY KEY (id)
+); 
+"""
+
+table_conditions = """
+create table if not exists production_conditions (
+    id int auto_increment,
+    icd10_code varchar(255) default null unique,
+    icd10_description varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+
+
+
+table_patient_medications = """
+create table if not exists production_patient_medications (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    med_ndc varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (med_ndc) REFERENCES production_medications(med_ndc) ON DELETE CASCADE
+); 
+"""
+
+
+table_patient_conditions = """
+create table if not exists production_patient_conditions (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    icd10_code varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (icd10_code) REFERENCES production_conditions(icd10_code) ON DELETE CASCADE
+); 
+"""
+
+
+db_gcp.execute(table_patients)
+db_gcp.execute(table_medications)
+db_gcp.execute(table_conditions)
+db_gcp.execute(table_patient_medications)
+db_gcp.execute(table_patient_conditions)
+
+
+# get tables from db_gcp
+gcp_tables = db_gcp.table_names()
